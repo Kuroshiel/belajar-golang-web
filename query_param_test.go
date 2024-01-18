7f6d7a3923c351e0468261ff1a0c6f4cfdec7b15
Kuroshiel <alifianfikry@gmail.com>
2024-01-18 16:56:06 +0700

    Learn Query Parameter Golang Web

---
+++ query_param_test.go
@@ -0,0 +1,78 @@
+package belajargolangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+// Query Parameter Golang Web
+
+func SayHello(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprint(writer, "Hello")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello?name=Eko", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHello(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
+
+// Multiple Query Parameter Golang Web
+
+func MultipleQueryParameter(writer http.ResponseWriter, request *http.Request) {
+	firstName := request.URL.Query().Get("first_name")
+	lastName := request.URL.Query().Get("last_name")
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet,
+		"http://localhost:8080/hello?first_name=Eko&last_name=Khannedy", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleQueryParameter(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
+
+// Multiple Value Query Parameter Golang Web
+
+func MultipleParameterValues(writer http.ResponseWriter, request *http.Request) {
+	query := request.URL.Query()
+	names := query["name"]
+	fmt.Fprint(writer, strings.Join(names, " "))
+}
+
+func TestMultipleParamaterValues(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet,
+		"http://localhost:8080/hello?name=Eko&name=Kurniawan&name=Khannedy", nil)
+	recorder := httptest.NewRecorder()
+
+	MultipleParameterValues(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
