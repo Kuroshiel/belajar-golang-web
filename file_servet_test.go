29492cbcc7f9301040ee2340ec95c477edd3c340
Kuroshiel <alifianfikry@gmail.com>
2024-01-19 16:22:29 +0700

    Learn File Server Golang Web

---
+++ file_servet_test.go
@@ -0,0 +1,51 @@
+package belajargolangweb
+
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+	"testing"
+)
+
+// File Server Golang Web
+
+func TestFileServer(t *testing.T) {
+	directory := http.Dir("./resources")
+	fileServer := http.FileServer(directory)
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// File Server Menggunakan Embed Golang Web
+
+//go:embed resources
+var resources embed.FS
+
+func TestFileServerGoEmbed(t *testing.T) {
+	directory, _ := fs.Sub(resources, "resources")
+	fileServer := http.FileServer(http.FS(directory))
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
