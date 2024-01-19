c9b4ffe05e8869d513ca338ed9c8744e6fc7eecb
Kuroshiel <alifianfikry@gmail.com>
2024-01-19 14:05:43 +0700

    Learn Response Code Golang Web

---
+++ response_code_test.go
@@ -0,0 +1,96 @@
+package belajargolangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+// Response Code Golang Web
+
+func ResponseCode(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+
+	if name == "" {
+		writer.WriteHeader(http.StatusBadRequest) // 400 = Bad Request
+		fmt.Fprint(writer, "Name Is Empty")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestResponseCodeInvalid(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func TestResponseCodeValid(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/name=Eko", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+// Response Code menggunaksan Form Posy Golang Web
+
+func FormPostResponseCode(writer http.ResponseWriter, request *http.Request) {
+	name := request.PostFormValue("name")
+
+	if name == "" {
+		writer.WriteHeader(http.StatusBadRequest) // 400 = Bad Request
+		fmt.Fprint(writer, "Name Is Empty")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+
+	contentType := request.Header.Get("content-type")
+	fmt.Fprint(writer, "\n"+contentType)
+
+}
+
+func TestFormPostResponseCodeInvalid(t *testing.T) {
+	requestBody := strings.NewReader("")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+
+	FormPostResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func TestFormPostResponseCodeValid(t *testing.T) {
+	requestBody := strings.NewReader("name=Kurniawan")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	recorder := httptest.NewRecorder()
+
+	FormPostResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
